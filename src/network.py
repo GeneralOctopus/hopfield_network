086ebd24c18d22d1971c285b8a12dd033cea92e3
Bartosz Maciej Jablonka <bartosz.maciej.jablonka@gmail.com>
2018-01-05 14:33:08 +0100

    Fix condition and comment old code

--- src/network.py
+++ src/network.py
@@ -5,99 +5,99 @@ from PIL import Image
 
 np.set_printoptions(threshold=np.inf)
 
-DIR_NAME = '9x9'
-N = 9
-DIR_NAME = 'converted_road_signs'
-N = 120
-
-patterns=[]
-size = 0
-weight_matrix = None
-list_of_files = None
-
-
-def train_hebb():
-    global patterns
-    global weight_matrix
-    global list_of_files
-    weight_matrix = zeros(shape=(N*N, N*N), dtype=float)
-    i = 0
-    for p in list_of_files:
-        i+= 1
-        pattern = convert_file_to_pattern(p)
-        o = outer(pattern,pattern)
-        weight_matrix += o
-        print ("trained %d of %d" % (i, len(list_of_files)))
-        del pattern
-        del o
-    weight_matrix /= len(list_of_files)
-    weight_matrix = np.asmatrix(weight_matrix)
-
-
-def get_list_of_files():
-    l = []
-    for f in os.listdir('../'+DIR_NAME):
-        f = '../'+DIR_NAME+'/'+f
-        if os.path.isfile(f):
-            img = Image.open(f)
-            if img.height * img.width <= N*N:
-                l.append(f)
-                print(f)
-    return l
-
-
-def convert_file_to_pattern(f):
-    img = Image.open(f)
-    img = img.convert('1')
-    i = []
-
-    for x in range(0, img.height):
-        for y in range(0, img.width):
-            i.append(1.0 if img.getpixel((y,x)) & 0x1 else -1.0)  # converted image has 255 if black, 0 if white)
-    if len(i) < N*N:
-        i += [-1] * (N*N - len(i))
-    return np.asarray(i, dtype=float)
-
-
-def test_images(output_dir='/../recognized/'):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    for im in list_of_files:
-
-        p = convert_file_to_pattern(im)
-        o = recognize(p)
-        print(im)
-
-        i = []
-        for x in range(0, N*N):
-            i.append((0, 0, 0) if o[0][x] < 0 else (255, 255, 255))
-
-        im1 = Image.new('RGB', (N,N))
-        im1.putdata(i)
-        im1.save(output_dir + os.path.basename(im))
-
-
-def recognize(pattern, max_steps=5):
-    global weight_matrix
-    for i in range(0, max_steps):
-        changes = False
-        pattern = np.asarray(np.dot(pattern, weight_matrix))
-
-        for j in range(0, N*N):
-            if 1 > pattern[0][j] > 0:
-                changes = True
-                pattern[0][j] = 1
-            elif -1 < pattern[0][j] < 0:
-                changes = True
-                pattern[0][j] = -1
-
-        print ("changes", changes)
-        if not changes:
-            print("stabilized after:", i)
-            return pattern
-
-    return pattern
+#DIR_NAME = '9x9'
+#DIR_NAME = 'converted_road_signs'
+#N = 120
+#N = 30
+#
+#patterns=[]
+#size = 0
+#weight_matrix = None
+#list_of_files = None
+#
+#
+#def train_hebb():
+#    global patterns
+#    global weight_matrix
+#    global list_of_files
+#    weight_matrix = zeros(shape=(N*N, N*N), dtype=float)
+#    i = 0
+#    for p in list_of_files:
+#        i+= 1
+#        pattern = convert_file_to_pattern(p)
+#        o = outer(pattern,pattern)
+#        weight_matrix += o
+#        print ("trained %d of %d" % (i, len(list_of_files)))
+#        del pattern
+#        del o
+#    weight_matrix /= len(list_of_files)
+#    weight_matrix = np.asmatrix(weight_matrix)
+#
+#
+#def get_list_of_files():
+#    l = []
+#    for f in os.listdir('../'+DIR_NAME):
+#        f = '../'+DIR_NAME+'/'+f
+#        if os.path.isfile(f):
+#            img = Image.open(f)
+#            if img.height * img.width <= N*N:
+#                l.append(f)
+#                print(f)
+#    return l
+#
+#
+#def convert_file_to_pattern(f):
+#    img = Image.open(f)
+#    img = img.convert('1')
+#    i = []
+#
+#    for x in range(0, img.height):
+#        for y in range(0, img.width):
+#            i.append(1.0 if img.getpixel((y,x)) & 0x1 else -1.0)  # converted image has 255 if black, 0 if white)
+#    if len(i) < N*N:
+#        i += [-1] * (N*N - len(i))
+#    return np.asarray(i, dtype=float)
+#
+#
+#def test_images(output_dir='/../recognized/'):
+#    if not os.path.exists(output_dir):
+#        os.makedirs(output_dir)
+#
+#    for im in list_of_files:
+#
+#        p = convert_file_to_pattern(im)
+#        o = recognize(p)
+#        print(im)
+#
+#        i = []
+#        for x in range(0, N*N):
+#            i.append((0, 0, 0) if o[0][x] < 0 else (255, 255, 255))
+#
+#        im1 = Image.new('RGB', (N,N))
+#        im1.putdata(i)
+#        im1.save(output_dir + os.path.basename(im))
+#
+#
+#def recognize(pattern, max_steps=5):
+#    global weight_matrix
+#    for i in range(0, max_steps):
+#        changes = False
+#        pattern = np.asarray(np.dot(pattern, weight_matrix))
+#
+#        for j in range(0, N*N):
+#            if 1 > pattern[0][j] > 0:
+#                changes = True
+#                pattern[0][j] = 1
+#            elif -1 < pattern[0][j] < 0:
+#                changes = True
+#                pattern[0][j] = -1
+#
+#        print ("changes", changes)
+#        if not changes:
+#            print("stabilized after:", i)
+#            return pattern
+#
+#    return pattern
 
 
 
@@ -127,7 +127,7 @@ class NeuronNetwork:
                     new_image += [-1] * (self.size_x*self.size_y - len(new_image))
         return np.asarray(new_image, dtype=float)
 
-    def convert_file_to_pattern(self, image):
+    def convert_file_to_pattern(self, image_path):
         image = Image.open(image_path)
         image = self._convert_image_size(image)
         image = self._convert_image_to_binary(image, 128)
@@ -135,7 +135,6 @@ class NeuronNetwork:
 
     def load_patterns(self):
         print "Loading patterns, please wait..."
-        print self.path_to_patterns 
         for image in os.listdir(self.path_to_patterns):
             image_path = self.path_to_patterns + image
             print "Loading pattern:", image_path
@@ -159,23 +158,14 @@ class NeuronNetwork:
         self.weight_matrix = np.asmatrix(self.weight_matrix)
         print "End learning by hebb method..."
 
-#def pseudo_inversion():
-#    global list_of_files
-#    global weight_matrix
-#    x = []
-#    for i, f in enumerate(list_of_files):
-#        x.append(convert_file_to_pattern(f))
-#
-#    x = np.asmatrix(x, dtype=float).transpose()
-#    weight_matrix = np.dot(x, np.linalg.pinv(x))
-
     def train_network_by_pseudo_inversion(self):
         print "Clear weight matrix before learning..."
         self.zeros_weight_matrix()
         print "Start learning by pseudo inversion..."
         self.x = []
         for index, pattern_path in enumerate(self.list_of_patterns):
-            self.x.append(convert_file_to_pattern(pattern_path))
+            print pattern_path
+            self.x.append(self.convert_file_to_pattern(pattern_path))
 
         self.x = np.asmatrix(self.x, dtype=float).transpose()
         self.weight_matrix = np.dot(self.x, np.linalg.pinv(self.x))
@@ -183,13 +173,9 @@ class NeuronNetwork:
 
     def recognize_image(self, image_path, max_steps=7):
         if os.path.isfile(image_path):
-            pattern = Image.open(image_path)
-            pattern = self.convert_file_to_pattern(pattern)
-
+            pattern = self.convert_file_to_pattern(image_path)
             for i in range(0, max_steps):
                 changes = False
-                print self.weight_matrix[0]
-                print self.weight_matrix[450]
                 pattern = np.asarray(np.dot(pattern, self.weight_matrix))
 
                 for j in range(0, self.size_x*self.size_y):
@@ -203,13 +189,17 @@ class NeuronNetwork:
                 print ("changes", changes)
                 if not changes:
                     print("stabilized after:", i)
-                i = []
-                for x in range(0, self.size_x*self.size_y):
-                    i.append((0, 0, 0) if pattern[0][x] < 128 else (255, 255, 255))
 
-                im1 = Image.new('RGB', (self.size_x, self.size_y))
-                im1.putdata(i)
-                im1.save(os.path.abspath(image_path) + os.path.basename(image_path)[::-7] + "_recognize.png" )
+            i = []
+
+            for x in range(0, self.size_x*self.size_y):
+#                    i.append(0  if pattern[0][x] < 128 else 255)
+                i.append((0, 0, 0)  if pattern[0][x] < 0 else (255, 255, 255))
+
+            
+            im1 = Image.new('RGB', (self.size_x, self.size_y))
+            im1.putdata(i)
+            im1.save(os.path.abspath(image_path) + os.path.basename(image_path)[::-7] + "_recognize.png" )
         
     def test_image(self, image_path):
         print "test image"
@@ -235,7 +225,7 @@ class NeuronNetwork:
 
     def test_network(self, input_directory):
         print "Testing network size", str(self.size_x) + "x" + str(self.size_y) + "..."
-        self.test_cut_images(input_directory)
+#        self.test_cut_images(input_directory)
         self.test_noised_images(input_directory)
 
     def zeros_weight_matrix(self):
@@ -264,7 +254,7 @@ def main():
     ps_inv_network = NeuronNetwork(patterns_directory, size, size)
     ps_inv_network.train_network_by_pseudo_inversion()
 
-#    ps_inv_network.test_network(tests_directories)
+    ps_inv_network.test_network(tests_directories)
 #    test_images(output_dir='../recognized/hebb/')
 #    print("END hebb")
 
